ba134934ffcadbae8f823016f45f0c6b7ba115e2
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2021-12-21 17:02:56 +0900

    fix

--- tests/OptionsSourceGenerator.TestClassLibrary/Class1.cs
+++ tests/OptionsSourceGenerator.TestClassLibrary/Class1.cs
@@ -4,8 +4,6 @@ namespace OptionsSourceGenerator.TestClassLibrary
 {
     public class Class1
     {
-        global::Microsoft.CodeAnalysis.Diagnostics.AnalyzerConfigOptions options;
-        OptionsSourceGenerator.Options a = new(new());
-        a
+        OptionsSourceGenerator.Options a;
     }
 }
